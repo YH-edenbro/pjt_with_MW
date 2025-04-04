3ec8315ef49c19e9835fd23e0d6217a3cea4450f
YH-edenbro <hyun96227@gmail.com>
2025-04-04 13:19:47 +0900

    진짜틀완성

--- crawlings/models.py
+++ crawlings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Jusik(models.Model):
+    company = models.CharField(max_length=50)
+    copany_code = models.CharField(max_length=50)
+    comment = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
